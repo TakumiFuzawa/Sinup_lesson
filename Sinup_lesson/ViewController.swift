68be7051a9272f223e050ae81dcdddfbf823ce6a
github TakumiFuzawa <futaku0811@gmail.com>
2020-02-27 11:04:39 +0900

    Initial Commit

---
+++ Sinup_lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sinup_lesson
+//
+//  Created by Takumi Fuzawa on 2020/02/27.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
